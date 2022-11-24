fd7cbcd55bb75c0730131a62671e3bb772f5dfad
Mayla <mayumi.mayla@gmail.com>
2022-11-24 20:08:12 -0300

    Semifinal

---
+++ JS/formulario.js
@@ -0,0 +1,24 @@
+document.querySelector("#botaoEnviar").addEventListener("click", validarFormulario)
+function validarFormulario() {
+    const nome = document.getElementById("nome")
+    const email = document.getElementById("email")
+    const text = document.getElementById("validationTextarea")
+    
+    if (
+        document.getElementById("nome").value != "" &&
+        document.getElementById("email").value != "" && 
+        document.getElementById("validationTextarea").valeu != ""
+    ) {
+        // const nome = document.getElementById("nome")
+        // const email = document.getElementById("email")
+        // const text = document.getElementById("validationTextara")
+        alert("Prontinho! Você receberá um e-mail com os próximos passos.")
+        
+        return;
+    } else {
+
+        alert("Por favor, preencha os campos!")
+
+        return;
+    }
+};
---
+++ JS/javascript.js
@@ -0,0 +1,155 @@
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+/* A casa carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+//HOME EFEITO DE APARECER
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #historia .image, #historia .text,
+  #cardapio header, #cardapio .image,
+  #acasa header, 
+  #contato .text,
+  footer .item, footer .social
+  `,
+  { interval: 100 }
+)
+
+//SOBRE
+scrollReveal.reveal(
+  `#sobre .image, #sobre .text,
+  #services-sobre .card
+  `,
+  { interval: 100 }
+)
+
+//CARDAPIO
+scrollReveal.reveal(
+  `#cardapio .text,
+  .direita .text, .direita .image,
+  .esquerda .text, .esquerda .image
+
+  `,
+  { interval: 100 }
+)
+
+//A CASA
+scrollReveal.reveal(
+  `#acasa .text,
+  #caroseul .container,
+  #inscricao .title
+  `,
+  { interval: 100 }
+)
+
+//CONTATO
+scrollReveal.reveal(
+  `#contato .text,
+  #vidas .text, #vidas .image
+  `,
+  { interval: 100 }
+)
+
+
+
+
+/* Botão voltar para o topo */
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (window.scrollY >= 520) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
+
+
+
+///
+
+
